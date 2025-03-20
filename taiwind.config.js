2a4016cc320779d19c4ea03ad167a7976c319998
Jeimy Marcela Mateus Ariza <jeimymateus1@gmail.com>
2025-03-20 18:01:48 -0500

    Iniciando

---
+++ taiwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+    darkMode: 'class', // o 'media' si prefieres que se active según la preferencia del sistema
+    theme: {
+      extend: {
+        colors: {
+          // Ejemplo de colores personalizados
+          darkBg: '#86198F',
+          darkText: '#000000',
+        },
+      },
+    },
+    plugins: [],
+  }
